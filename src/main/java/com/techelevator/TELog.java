02acd9e3704d9c475dea3d07237a026b3b6e6571
Anna <lalonglois@gmail.com>
2022-12-12 17:02:29 -0500

    finalPush

--- src/main/java/com/techelevator/TELog.java
+++ src/main/java/com/techelevator/TELog.java
@@ -2,19 +2,22 @@ package com.techelevator;
 
 import com.techelevator.util.TELogException;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
 public class TELog extends RuntimeException {
     public static void log(String message) {
-        PrintWriter dataOut = null;
-        try {
-            dataOut = new PrintWriter("log/search.log");
+        //PrintWriter dataOut = null;
+        String fileName = "log/search.log";
+        File dataFile = new File(fileName);
+        try (PrintWriter dataOutput = new PrintWriter(dataFile)){
+            dataOutput.println(message);
         } catch (Exception e) {
            throw new TELogException("An error has occurred.");
         }
-        dataOut.println(message);
-            dataOut.close();
+//        dataOut.println(message);
+//            dataOut.close();
 
 
     }
