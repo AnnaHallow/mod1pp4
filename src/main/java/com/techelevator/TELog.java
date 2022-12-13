379b7a9af71e64590ba39c1e99b1790495850e98
Warren <warren.pierson05@gmail.com>
2022-12-13 13:33:11 -0800

    Fixed TELog

--- src/main/java/com/techelevator/TELog.java
+++ src/main/java/com/techelevator/TELog.java
@@ -9,7 +9,7 @@ import java.io.PrintWriter;
 public class TELog extends RuntimeException {
     public static void log(String message) {
         //PrintWriter dataOut = null;
-        String fileName = "log/search.log";
+        String fileName = "/Users/warren/Desktop/meritamerica/repos/PairProgramming/module-1-pair-programming-4/logs/search.log";
         File dataFile = new File(fileName);
         try (PrintWriter dataOutput = new PrintWriter(dataFile)){
             dataOutput.println(message);
