827a2cdc90b31dd8b3da2dc35ce14f36a59e36a0
Warren <warren.pierson05@gmail.com>
2022-12-13 16:19:00 -0800

    fixed TELog file path

--- src/main/java/com/techelevator/TELog.java
+++ src/main/java/com/techelevator/TELog.java
@@ -5,18 +5,20 @@ import com.techelevator.util.TELogException;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class TELog extends RuntimeException {
     public static void log(String message) {
+
         //PrintWriter dataOut = null;
-        String fileName = "/Users/warren/Desktop/meritamerica/repos/PairProgramming/module-1-pair-programming-4/logs/search.log";
+        String fileName = "logs/search.log";
         File dataFile = new File(fileName);
         try (PrintWriter dataOutput = new PrintWriter(dataFile)){
             dataOutput.println(message);
         } catch (Exception e) {
            throw new TELogException("An error has occurred.");
         }
-//        dataOut.println(message);
 //            dataOut.close();
 
 
